c38c20a5b1cfe8ca0192d0fb5588e3a8d4f1b356
vdomos <domos.vesta@gmail.com>
2016-01-07 17:36:03 +0100

    PEP8 pass

--- bin/rfxbnz.py
+++ bin/rfxbnz.py
@@ -42,66 +42,66 @@ import threading
 
 
 class RfxBnzManager(XplPlugin):
-	""" 
-	"""
+    """
+    """
 
-	def __init__(self):
-		""" Init plugin
-		"""
-		XplPlugin.__init__(self, name='rfxbnz')
+    def __init__(self):
+        """ Init plugin
+        """
+        XplPlugin.__init__(self, name='rfxbnz')
 
-		# check if the plugin is configured. If not, this will stop the plugin and log an error
-		if not self.check_configured():
-			return
-			
-		# Configuration
-		host = self.get_config("host")						 # host where rfxcomrx run
-		if host == None:
-			self.log.error('### Host is not configured, exiting') 
-			self.force_leave()
-			return
+        # check if the plugin is configured. If not, this will stop the plugin and log an error
+        if not self.check_configured():
+            return
 
-		self.lastTimestampHbeat = time.time()
-		
-		# Create listeners
-		self.log.info("==>  Creating listener for rfxbnz") 
-		Listener(self.rfxbnz_stat_cb, self.myxpl, {'xplsource':'bnz-rfxcomrx.' + host, 'xpltype': 'xpl-stat', 'schema': 'hbeat.app'})
+        # Configuration
+        host = self.get_config("host")                         # host where rfxcomrx run
+        if host is None:
+            self.log.error('### Host is not configured, exiting')
+            self.force_leave()
+            return
 
-		self.log.info("==> Launch thread to rfxbnz_chk_hbeat")
-		threads = {}
-		thr_name = "rfxbnz_chk"
-		threads[thr_name] = threading.Thread(None, 
-								self.rfxbnz_chk_hbeat,
-								thr_name,
-									(self.log,
-									self.get_stop()),
-								{})
-		threads[thr_name].start()
-		self.register_thread(threads[thr_name])
+        self.lastTimestampHbeat = time.time()
 
-		self.ready()
+        # Create listeners
+        self.log.info("==>  Creating listener for rfxbnz")
+        Listener(self.rfxbnz_stat_cb, self.myxpl, {'xplsource': 'bnz-rfxcomrx.' + host, 'xpltype': 'xpl-stat', 'schema': 'hbeat.app'})
+
+        self.log.info("==> Launch thread to rfxbnz_chk_hbeat")
+        threads = {}
+        thr_name = "rfxbnz_chk"
+        threads[thr_name] = threading.Thread(None,
+                                            self.rfxbnz_chk_hbeat,
+                                            thr_name,
+                                            (self.log,
+                                                self.get_stop()),
+                                            {})
+        threads[thr_name].start()
+        self.register_thread(threads[thr_name])
+
+        self.ready()
 
 
+    def rfxbnz_stat_cb(self, message):
+        """
+        Callback heartbeat rfxcomrx messages
+        """
+        self.log.debug("==> Call rfxbnz_stat_cb")
+        self.lastTimestampHbeat = time.time()                      # time.time() -> 1324305354.0857329
 
-	def rfxbnz_stat_cb(self, message):
-		""" 
-		"""
-		self.log.debug("==> Call rfxbnz_stat_cb")
-		self.lastTimestampHbeat = time.time()		  			# time.time() -> 1324305354.0857329
-		
-		
-	def rfxbnz_chk_hbeat(self, log, stop):
-		""" 
-		"""
-		self.log.debug("==> Call rfxbnz_chk_hbeat thread")
-		while not self._stop.isSet():
-			self.log.debug("==> Check if hbeat.app of 'rfxcomrx' process is too old ...")
-			if time.time() - self.lastTimestampHbeat > 630:
-				self.log.error('### rfxcomrx.pl not alive, exiting') 
-				self.force_leave()
-			time.sleep(120)
-			
+
+    def rfxbnz_chk_hbeat(self, log, stop):
+        """
+        Check if heartbeat to old
+        """
+        self.log.debug("==> Call rfxbnz_chk_hbeat thread")
+        while not self._stop.isSet():
+            self.log.debug("==> Check if hbeat.app of 'rfxcomrx' process is too old ...")
+            if time.time() - self.lastTimestampHbeat > 630:
+                self.log.error('### rfxcomrx.pl not alive, exiting')
+                self.force_leave()
+            time.sleep(120)
+
 
 if __name__ == "__main__":
-	RfxBnzManager()
-
+    RfxBnzManager()
